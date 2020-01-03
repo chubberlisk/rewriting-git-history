e93f70cfecd4181a32c8139e8249c9ddbd9f94fc
Wen Ting Wang <chubberlisk@outlook.com>
2020-01-03 15:05:50 +0000

    Add ability to display scoring up to deuce

---
+++ lib/tennis.rb
@@ -0,0 +1,22 @@
+class Tennis
+  POINTS_TO_SCORE = %w[Love Fifteen Thirty Fourty].freeze
+
+  def initialize
+    @player_one_points = 0
+    @player_two_points = 0
+  end
+
+  def win_point(player)
+    player_one?(player) ? @player_one_points += 1 : @player_two_points += 1
+  end
+
+  def score
+    "#{POINTS_TO_SCORE[@player_one_points]}-#{POINTS_TO_SCORE[@player_two_points]}"
+  end
+
+  private
+
+  def player_one?(player)
+    player == :player_one
+  end
+end
